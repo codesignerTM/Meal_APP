506f7c59d37ed4ec7322969a6c914bc948a50089
Tamás <codesigner.hu@gmail.com>
2019-12-27 23:08:46 +0100

    Initial commit

---
+++ screens/MealDetailsScreen.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const MealDetailsScreen = props => {
+  return (
+    <View styles={style.screen}>
+      <Text>The heldsdsd lo!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
+
+export default MealDetailsScreen;
