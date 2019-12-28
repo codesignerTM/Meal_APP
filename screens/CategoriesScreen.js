2f80824edf601328336d77f03d620f4dca3216d9
Tamás <codesigner.hu@gmail.com>
2019-12-28 11:45:54 +0100

    improving cetegories screen

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,19 +1,49 @@
 import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
+  Platform
+} from "react-native";
+
+import { CATEGORIES } from "../data/dummy-data";
+import Colors from "../constants/colors";
 
 const CategoryScreen = props => {
   console.log(props);
-  return (
-    <View style={styles.screen}>
-      <Text>The CategoryScreen!</Text>
-      <Button
-        title="Go to meals"
+  const renderGridItem = itemData => {
+    return (
+      <TouchableOpacity
+        style={styles.gridItem}
         onPress={() => {
           props.navigation.navigate({ routeName: "CategoryMeals" });
         }}
-      />
-    </View>
+      >
+        <View>
+          <Text>{itemData.item.title}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+  return (
+    <FlatList
+      keyExtractor={(item, index) => item.id}
+      data={CATEGORIES}
+      renderItem={renderGridItem}
+      numColumns={2}
+    />
   );
+};
+
+CategoryScreen.navigationOptions = {
+  headerTitle: "Meal Categories",
+  headerStyle: {
+    backgroundColor: Platform.OS === "android" ? Colors.primaryColor : "#fff"
+  },
+  headerTintColor: Platform.OS === "android" ? "#fff" : Colors.primaryColor
 };
 
 const styles = StyleSheet.create({
@@ -21,6 +51,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
+  },
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150
   }
 });
 
