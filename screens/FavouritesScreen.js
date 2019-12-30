12e617fc4bd65d624a701f331c3774075255eea6
Tamás <codesigner.hu@gmail.com>
2019-12-30 13:35:19 +0100

    improving favourites screen

--- screens/FavouritesScreen.js
+++ screens/FavouritesScreen.js
@@ -1,20 +1,14 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import MealList from "../components/MealList";
+import { MEALS } from "../data/dummy-data";
 
 const FavouritesScreen = props => {
-  return (
-    <View style={styles.screen}>
-      <Text>The FavouritesScreen!</Text>
-    </View>
-  );
+  const MealId = MEALS.filter(meal => meal.id === "m1" || meal.id === "m2");
+  return <MealList displayedMeals={MealId} navigation={props.navigation} />;
 };
 
-const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
-  }
-});
+FavouritesScreen.navigationOptions = {
+  headerTitle: "Your favourites"
+};
 
 export default FavouritesScreen;
