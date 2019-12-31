bc65d120270e0da1d64d6212630ce20d1c2cb241
Tamás <codesigner.hu@gmail.com>
2019-12-31 11:56:35 +0100

    Adding default text component

--- components/MealItem.js
+++ components/MealItem.js
@@ -6,6 +6,7 @@ import {
   StyleSheet,
   ImageBackground
 } from "react-native";
+import DefaultText from "./DefaultTextComponent";
 
 const MealItem = props => {
   return (
@@ -18,16 +19,16 @@ const MealItem = props => {
               style={styles.bcgImage}
             >
               <View style={styles.titleContainer}>
-                <Text style={styles.title} numberOfLines={1}>
+                <DefaultText style={styles.title} numberOfLines={1}>
                   {props.title}
-                </Text>
+                </DefaultText>
               </View>
             </ImageBackground>
           </View>
           <View style={{ ...styles.mealRow, ...styles.mealDetail }}>
-            <Text>{props.duration} m</Text>
-            <Text>{props.complexity.toUpperCase()}</Text>
-            <Text>{props.affordability.toUpperCase()}</Text>
+            <DefaultText>{props.duration} m</DefaultText>
+            <DefaultText>{props.complexity.toUpperCase()}</DefaultText>
+            <DefaultText>{props.affordability.toUpperCase()}</DefaultText>
           </View>
         </View>
       </TouchableOpacity>
