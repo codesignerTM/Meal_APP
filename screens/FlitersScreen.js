3a84dc01a92db6c9a679161e08ceda60770ae152
Tamás <codesigner.hu@gmail.com>
2020-01-01 11:26:53 +0100

    Adding switches to filter screen

--- screens/FlitersScreen.js
+++ screens/FlitersScreen.js
@@ -1,13 +1,56 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Switch, Platform } from "react-native";
 
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import HeaderButton from "../components/HeaderButton";
+import Colors from "../constants/colors";
+
+const FilterSwitch = props => {
+  return (
+    <View style={styles.filterContainer}>
+      <Text>{props.label}</Text>
+      <Switch
+        value={props.value}
+        onValueChange={props.onValueChange}
+        trackColor={{ true: Colors.primaryColor }}
+        thumbColor={Platform.OS === "android" ? Colors.primaryColor : ""}
+      />
+    </View>
+  );
+};
 
 const FilterScreen = props => {
+  const [isGlutenFree, setIsGlutenFree] = useState(false);
+  const [isLactozeFree, setIsLactozeFree] = useState(false);
+  const [isVegan, setIsVegan] = useState(false);
+  const [isVegetarian, setIsVegetarian] = useState(false);
+
   return (
     <View style={styles.screen}>
-      <Text>The filter screen!</Text>
+      <Text style={styles.title}>Available Filters / Restrictions</Text>
+      <FilterSwitch
+        label="Gluten-free"
+        onValueChange={newValue => setIsGlutenFree(newValue)}
+        value={isGlutenFree}
+      />
+
+      <FilterSwitch
+        label="Lactoze-free"
+        onValueChange={newValue => setIsLactozeFree(newValue)}
+        value={isLactozeFree}
+      />
+
+      <FilterSwitch
+        label="Vegan"
+        onValueChange={newValue => setIsVegan(newValue)}
+        value={isVegan}
+      />
+
+      <FilterSwitch
+        label="Vegetarian"
+        onValueChange={newValue => setIsVegetarian(newValue)}
+        value={isVegetarian}
+      />
     </View>
   );
 };
@@ -32,8 +75,20 @@ FilterScreen.navigationOptions = navigationData => {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: "center",
     alignItems: "center"
+  },
+  filterContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "80%",
+    marginVertical: 10
+  },
+  title: {
+    fontFamily: "open-sans-bold",
+    fontSize: 22,
+    margin: 20,
+    textAlign: "center"
   }
 });
 
