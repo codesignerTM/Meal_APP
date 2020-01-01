9c8edb4b56f589c2278f3f5b480b8747c510f476
Tamás <codesigner.hu@gmail.com>
2020-01-01 21:29:16 +0100

    adding fall back text

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { View, StyleSheet } from "react-native";
 
 import { CATEGORIES } from "../data/dummy-data";
 import MealList from "../components/MealList";
+import DefaultText from "../components/DefaultTextComponent";
 
 const CategoryMealScreen = props => {
   const catId = props.navigation.getParam("categoryId");
@@ -13,6 +15,15 @@ const CategoryMealScreen = props => {
   const displayedMeals = availabeMeals.filter(
     meal => meal.categoryIds.indexOf(catId) >= 0
   );
+
+  if (displayedMeals.length === 0) {
+    return (
+      <View style={styles.content}>
+        <DefaultText>No meals find, check your filters!</DefaultText>
+      </View>
+    );
+  }
+
   return (
     <MealList displayedMeals={displayedMeals} navigation={props.navigation} />
   );
@@ -25,5 +36,13 @@ CategoryMealScreen.navigationOptions = navigationData => {
     headerTitle: selectedCategory.title
   };
 };
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
 
 export default CategoryMealScreen;
